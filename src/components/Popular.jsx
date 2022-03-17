b5ab4beaa4a22cdc06762a55e161b775ffd1ccf5
Naweli Verma <Naweli.Verma@propertyloop.co.uk>
2022-03-17 12:36:43 +0530

    installed pacakages and added styled component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,10 +1,12 @@
 // import { useEffect, useState } from "react";
-import {useQuery} from "react-query";
-import axios from 'axios';
+import { useQuery } from "react-query";
+import axios from "axios";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
-
-// traditional method starts
+  // traditional method starts
   // const [popular, setPopular] = useState([]);
 
   // const getPopular = async () => {
@@ -23,32 +25,86 @@ function Popular() {
 
   // traditional method ends
 
+  const { isLoading, data } = useQuery("popular", () => {
+    return axios
+      .get(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+      )
+      .then((res) => res.data);
+  });
 
-  const {isLoading,data} = useQuery('popular',()=>{
-  return axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`).then(res=>res.data);
-
-
-  })
-
-  if(isLoading)
-  {
-    return <h2>Loading...</h2>
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
-
-console.log({data})
   return (
-
     // <div>
     //   {popular.map((recipe) => {
     //     return <div key={recipe.id}>{recipe.title}</div>;
     //   })}
     // </div>
     <div>
-      {data?.recipes?.map(recipe=>{
-        return<div key={recipe.id}>{recipe.title}</div>
-      })}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide 
+        options={{
+          perPage:4,
+          arrows:false,
+          pagination:false,
+          drag:"free",
+          gap:"5rem"
+
+        }}>
+          {data?.recipes?.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position:relative;
+
+  img {
+    border-radius: 2rem;
+    position:absolute;
+    left:0;
+    width:100%;
+    height:100%;
+    object-fit:cover;
+  }
+
+  p{
+    position:absolute;
+    z-index:10;
+    left:50%;
+    bottom:0%;
+    transform:translate(-50%,0%);
+    color:white;
+    width:100%;
+    text-align:center;
+    font-weight:600;
+    font-size:1rem;
+    height:40%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+  }
+`;
 
 export default Popular;
